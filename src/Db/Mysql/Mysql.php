3c8bc181a3b99de7be4e938814da89b10a53d0ed
Macc Liu <maccliu@qq.com>
2017-09-08 23:33:27 +0800

    checkin

--- src/Db/Mysql/Mysql.php
+++ src/Db/Mysql/Mysql.php
@@ -10,12 +10,28 @@ use \PDO;
 use \Dida\Db;
 use \Dida\Db\SchemaInterface;
 use \Exception;
+use \Dida\Db\Mysql\MysqlBuilder;
 
 /**
  * Mysql
  */
 class Mysql extends Db implements SchemaInterface
 {
-    /* Mysql Schema Trait */
     use MysqlSchemaTrait;
+
+
+    public function table($table, $with_prefix = true)
+    {
+        // Gets the real table name
+        $realtable = ($with_prefix) ? $this->cfg['prefix'] . $table : $table;
+
+        // get the table information
+        $table_info = $this->workdir . '~SCHEMA' . DIRECTORY_SEPARATOR . $realtable . '.php';
+        if (!file_exists($table_info)) {
+            return false;
+        }
+
+        $t = new MysqlBuilder($this, $realtable);
+        return $t;
+    }
 }
