ddf2a4d2dc38616ffa452fb9df84685471e650de
Macc Liu <maccliu@qq.com>
2017-09-23 12:05:09 +0800

    dev

--- src/Db/Mysql/Mysql.php
+++ src/Db/Mysql/Mysql.php
@@ -20,21 +20,44 @@ class Mysql extends Db implements SchemaInterface
     use MysqlSchemaTrait;
 
 
-    public function table($table, $prefix = null)
+    public function __construct(array $cfg = array())
     {
-        // Gets the real table name
-        if (is_null($prefix)) {
+        parent::__construct($cfg);
+
+        /* charset option */
+        if (is_string($this->cfg['charset'])) {
+            switch (strtolower($this->cfg['charset'])) {
+                case 'utf8':
+                case 'utf-8':
+                case 'utf_8':
+                    $this->cfg['options'][PDO::MYSQL_ATTR_INIT_COMMAND] = 'SET NAMES utf8';
+                    break;
+                default:
+                    $this->cfg['options'][PDO::MYSQL_ATTR_INIT_COMMAND] = 'SET NAMES ' . $this->cfg['charset'];
+            }
+        } else {
+            unset($this->cfg['options'][PDO::MYSQL_ATTR_INIT_COMMAND]);
+        }
+
+        /* persistence option */
+        if ($this->cfg['persistence']) {
+            $this->cfg['options'][PDO::ATTR_PERSISTENT] = true;
+        } else {
+            unset($this->cfg['options'][PDO::ATTR_PERSISTENT]);
+        }
+    }
+
+
+    public function table($table, $prefix = null, $formal_prefix = null)
+    {
+        if ($prefix === null) {
             $prefix = $this->cfg['prefix'];
         }
-        $realtable =  $prefix . $table;
-
-        // the table defination file exists?
-        $table_def_file = $this->workdir . '~SCHEMA' . DIRECTORY_SEPARATOR . $realtable . '.php';
-        if (!file_exists($table_def_file)) {
-            return false;
+        if ($formal_prefix === null) {
+            $formal_prefix = $this->cfg['formal_prefix'];
         }
 
-        $t = new MysqlBuilder($this, $table, $prefix);
+        $t = new MysqlBuilder($this, $table, $prefix, $formal_prefix);
         return $t;
     }
 }
