7d482600da36af7190a1a1d14f03cf2f72c2c7f8
Macc Liu <maccliu@qq.com>
2017-10-16 14:26:16 +0800

    把Composer相关的部分都转移到dida-db/composer目录（和dev、master同一目录级别），因为vendor的代码不应该影响到我们这个库

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -8,5 +8,5 @@ require(__DIR__ . '/Autoloader.php');
 Autoloader::init();
 Autoloader::addPsr4('Dida\\Db', __DIR__ . '/../src/Db');
 
-require(__DIR__ . '/vendor/autoload.php');
+require(__DIR__ . '/../../composer/vendor/autoload.php');
 
